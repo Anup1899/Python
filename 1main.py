dbbcf1b876e06aa6a00182758bdb75613470c6db
Anup-1827 <anupalone.off@gmail.com>
2024-09-08 23:16:18 +0530

    Learning

---
+++ 1main.py
@@ -0,0 +1,34 @@
+import random # Used to generate random numbers in python
+
+# List in python
+# Used to store multiple values in a single variable
+food = ["pizza", "pasta", "burger", "chips"]
+dinner = random.choice(food)
+
+
+# Dictionary in python
+# Used to store key value pairs
+dict = {'name': "Anup", 'color': choices}
+
+
+# Function in python
+# Indentation is very important in python
+def get_choices():
+    player_choice = input("Enter a choice (rock, paper, scissors): ") # Taking Input from the user
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options) # Define a variable in python
+    # print(player_choice, computer_choice)
+    choices = {'player': player_choice, 'computer': computer_choice, }
+    return choices
+
+def check_win(player, computer):
+    
+
+
+choices = get_choices()
+print(choices)
+
+
+
+
+
